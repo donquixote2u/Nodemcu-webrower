439c561ad666d5ceca03e459c4a504dd3be68e72
donquixote2u <donquixote2u@gmail.com>
2021-07-17 11:20:48 +1200

    renamed:    OTA.lua -> OTA2.lua 	modified:   init.lua

--- init.lua
+++ init.lua
@@ -7,15 +7,12 @@ wifiTrys     = 0      -- reset counter of trys to connect to wifi
 NUMWIFITRYS  = 20    -- Maximum number of WIFI Testings while waiting for connection
 Log={}
 Loglim=30;
--- dofile("serial.lua")      -- load serial function
 serinitTimeout=5000       -- timer in ms
 serTimer=tmr.create()     -- create timer
 require("WifiConnect")
-dofile("webif.lua")
-function init_webserver()
-server()
-FSERIAL("ON")   -- init serial port
+function init_2()
+require("OTA2")
 end
 initTimeout=2000       --  timer in ms
 initTimer=tmr.create()  -- start timer
-initTimer:alarm(initTimeout,tmr.ALARM_SINGLE,function() checkConnection(init_webserver) end) 
+initTimer:alarm(initTimeout,tmr.ALARM_SINGLE,function() checkConnection(init_2) end) 
